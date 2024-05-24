20682e2149549b60048f3d5fe6876fbaae1600a9
TinySkillet <mirageaditya@gmail.com>
2024-05-24 10:36:52 +0545

    'day-4'

---
+++ js/lesson4.js
@@ -0,0 +1,28 @@
+// for loop
+
+for(let i = 1; i <= 5; i ++){
+    console.log(i)
+}
+
+
+for (let i = 10; i >= 0; i --){
+    console.log(i)
+}
+
+// while loop
+
+let count = 5;
+while(count >= -1){
+    console.log(count);
+    count --; 
+}
+
+// do while loop
+
+i = 0
+do {
+    console.log(i);
+    i ++;
+} while(i < 5)
+
+
