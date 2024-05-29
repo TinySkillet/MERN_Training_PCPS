0cfe3eea94954d20aeb39f9de4b097cce2f6de12
TinySkillet <mirageaditya@gmail.com>
2024-05-29 10:48:03 +0545

    Added lesson6.js

--- js/lesson5.js
+++ js/lesson5.js
@@ -10,11 +10,70 @@ console.log(subtract(12, 10))
 let multiply = (a, b) => {return a * b}
 console.log(multiply(4, 5))
 
+console.log('\n')
 // generator function
-// read yourself
 
-// read ES6
+function* func(){
+    yield 10;
+    yield 20;
+    yield 30;
+}
 
+let gen = func();
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+
+
+// natural number generator
+
+function* nextNatural(){
+    let natural_num = 1;
+
+    while (true){
+        yield natural_num++;
+    }
+}
+
+gen = nextNatural()
+
+for (i = 1; i <= 10; i++){
+    console.log(gen.next().value)
+}
+
+console.log('\n')
+
+
+let array = ['a', 'b', 'c']
+function* generator(arr){
+    let i = 0;
+    while (i < arr.length){
+        yield arr[i++]
+    }
+}
+
+const it = generator(array)
+
+c = it.next().value
+
+while (c){
+    console.log(c)
+    c = it.next().value
+}
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8]
+
+function* genV2(){
+    yield 0;
+    yield* arr;
+    yield 9;
+}
+
+for (value of genV2()){
+    console.log(value)
+}
+
+console.log('\nConstructor Functions')
 // constructor function
 
 function Person(name, age){
@@ -24,11 +83,6 @@ function Person(name, age){
 let john = new Person('John', 24)
 console.log(john)
 
-let person = {}
-person.name = 'John';
-person.age = 23;
-
-console.log(person)
 
 // Arrays
 
@@ -59,6 +113,43 @@ const personn = {
 console.log(personn.name)
 console.log(personn.age) 
 console.log(personn.hobbies) 
+
+
+// ES6
+console.log('\nES6')
+// Object literals
+
+const x = 1;
+const y = 2;
+const obj = {x, y, greet(){return 'Hello';}};
+
+console.log(obj.x)
+console.log(obj.y)
+console.log(obj.greet())
+
+// default parameters
+
+function greet(name = 'Harry'){
+    return `Hello ${name}`
+}
+
+console.log(greet())
+console.log(greet('Aditya'))
+
+
+// Rest and Spread Operators
+
+function sum(...args){
+    return args.reduce((a, b) => a + b, 0)
+}
+
+arr = [1, 2, 3]
+const newArr = [...arr, 4, 5]
+console.log(newArr)
+
+
+
+
 
 
 
---
+++ js/lesson6.js
@@ -0,0 +1,25 @@
+// constructor function
+
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+}
+
+let john = new Person('John', 25);
+console.log(john)
+
+// callback function
+
+/* function passed as an argument to another
+function and invoked within that function
+commonly used for asynchronous operations or event handling
+*/
+
+let data = 'string';
+console.log(data.codePointAt(1))
+
+'hiteshChoudhary'
+'js mastery'
+
+
+
